d967fa3e90ae8a7ae7820956e976d8ad15316c7f
wl19991215 <2483892293@qq.com>
2021-06-01 21:19:02 +0800

    分支修改3 增加hello master

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -3,5 +3,6 @@ package com.duyi;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("test change");
+        System.out.println("hello master");
     }
 }
